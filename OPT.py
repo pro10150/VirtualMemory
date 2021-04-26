94a314d072a6729eefae1ed1c59aa29260d8f3a9
pro10150 <pro10150@gmail.com>
2021-04-27 00:59:15 +0700

    add first page

--- OPT.py
+++ OPT.py
@@ -1,26 +1,37 @@
+
 from random import randint
 
 pageAddressing = []
 frameNum = input("Input frame: ")
 frame = []
-pivot = 0;
+pivot = 0
+processList = []
+pageFault = 0
 
-for i in range(12):
-    rand = randint(0,7)
-    x = frame.count(rand)
-    print(rand)
+for i in range(10):
+    rand = randint(0, 7)
+    processList.insert(i,rand)
+
+print(processList)
+
+for process in processList:
+    # rand = randint(0,7)
+    x = frame.count(process)
+    print(process)
     #print(pivot)
     #print(pivot == int(frameNum))
     if(len(frame) == int(frameNum)):
         #print(x == 0)
         if(x == 0):
             frame.pop(0)
+            pageFault += 1
         pivot = int(frameNum) - 6
-    pageAddressing.insert(i,rand)
     if(len(frame) < int(frameNum)):
         if(x == 0):
-            frame.insert(pivot,rand)
+            frame.insert(pivot,process)
             pivot = pivot + 1
     print(frame)
 
-print(pageAddressing)
+    print("page fault = {}".format(pageFault))
+
+
